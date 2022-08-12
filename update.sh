ea9346d2eb70550fe8246851cbf9d0ab03fb7e61
lyazj <seeson@pku.edu.cn>
2022-08-12 20:32:02 +0800

    Add `.desktop` and 256-icon.

--- update.sh
+++ update.sh
@@ -7,16 +7,23 @@ if [ ! $UID = 0 ]; then
 fi
 
 set -ve
+mkdir -p ${PREFIX}
 wget --continue --trust-server-names $(cat url.txt)
 ls firefox-*.tar.bz2 | sort --version-sort | head --lines=-1 | xargs rm -f
 ARCHIVE_NAME=$(ls firefox-*.tar.bz2)
 BASE_NAME=${ARCHIVE_NAME%.*.*}
 CWD=$(pwd)
 cd ${PREFIX}
-if [ -d "${BASE_NAME}" ]; then
-    exit 0
+if [ ! -d "${BASE_NAME}" ]; then
+    mkdir "${BASE_NAME}"
+    bzip2 -kdc "${CWD}/${ARCHIVE_NAME}" | tar -xC "${BASE_NAME}"
 fi
-mkdir "${BASE_NAME}"
-bzip2 -kdc "${CWD}/${ARCHIVE_NAME}" | tar -xC "${BASE_NAME}"
 ln -sfT "${BASE_NAME}/firefox" firefox
+mkdir -p bin
+ln -sf ../firefox/firefox bin
+mkdir -p share/applications
+cp "${CWD}"/firefox-default256.png share/applications
+ICON_FILE=$(pwd)/share/applications/firefox-default256.png
+sed -e "s/^Icon=.*$/Icon=${ICON_FILE//\//\\\/}/" \
+    "${CWD}"/firefox.desktop > share/applications/firefox.desktop
 ls -d firefox-* | sort --version-sort | head --lines=-1 | xargs rm -rf
